5002ac56d3ec4b13f4eceb5b78c85c62cbccad1c
Geist5000 <geist5000@gmail.com>
2021-12-25 01:27:30 +0100

    improved BuilderManager

--- src/main/java/de/g3sit/fastbridge/building/BuilderManager.java
+++ src/main/java/de/g3sit/fastbridge/building/BuilderManager.java
@@ -1,7 +1,9 @@
 package de.g3sit.fastbridge.building;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -9,35 +11,55 @@ import java.util.function.Consumer;
 public class BuilderManager {
 
     private static final int WORKER_COUNT = 5;
+    private static final int WORKER_DELAY = 10;
+    private static final int WORKER_SPEED = 1;
+    private Plugin plugin;
 
-    List<BuilderWorker> worker;
+    private final List<BuilderWorker> worker;
+    private final HashMap<BuildJob,Consumer<BuildJob>> buildJobFinishedCallbacks;
 
     public BuilderManager() {
         this.worker = new LinkedList<>();
+        this.buildJobFinishedCallbacks = new HashMap<>();
     }
 
 
     /**
-     * creates a new Worker if the maximum worker count isn't reached and returns the least busy worker
+     * creates a new Worker if the maximum worker count isn't reached and returns the least busy worker.
+     *
+     * Returned worker is always running
      * @return a BuilderRunnable instance, which is still running
      */
     private BuilderWorker getOrCreateRunnable() {
         if (worker.size() < WORKER_COUNT) {
-            BuilderWorker runnable = new BuilderWorker(this.worker::remove, this::buildJobFinished); // maybe create an own callback and don't use remove function of List
+            BuilderWorker runnable = new BuilderWorker(WORKER_SPEED,this.worker::remove, this::buildJobFinished); // maybe create an own callback and don't use remove function of List
             this.worker.add(runnable);
+            runnable.runTaskTimer(plugin,5, WORKER_DELAY);
             return runnable;
         }
+
+
         BuilderWorker minRunnable = null;
+        int minInstructionCount = 0;
         for (BuilderWorker builder :
                 this.worker) {
-            if (minRunnable == null || minRunnable.getInstructionCount() > builder.getInstructionCount()) {
+            int instructionCount = builder.getInstructionCount();
+            if (minRunnable == null || minInstructionCount > instructionCount) {
                 minRunnable = builder;
+                minInstructionCount = instructionCount;
             }
         }
         return minRunnable;
     }
 
+    public void queueBuildJob(BuildJob job, Consumer<BuildJob> onBuildJobFinishedCallback){
+        this.getOrCreateRunnable().addBuildJob(job);
+        this.buildJobFinishedCallbacks.put(job,onBuildJobFinishedCallback);
+    }
+
     private void buildJobFinished(BuildJob job){
         Bukkit.getLogger().info("finished BuildJob");
+        // call build job finished callback
+        this.buildJobFinishedCallbacks.remove(job).accept(job);
     }
 }
